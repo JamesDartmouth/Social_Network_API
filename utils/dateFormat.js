dbc874f68f56f51c2488be4f5bf170ade843e33b
JamesDartmouth <jamessung76@gmail.com>
2022-11-10 17:23:11 -0800

    added dateFormat

---
+++ utils/dateFormat.js
@@ -0,0 +1,43 @@
+
+module.exports = () => {
+
+    const now = new Date()
+    console.log(now)
+    
+    const year = now.getFullYear()
+    console.log(year)
+    
+    const month = now.getMonth() +1
+    console.log(month)
+    
+    const day = now.getDate()
+    console.log(day)
+    
+    const hours = now.getHours()
+    console.log(hours)
+    
+    const minutes = now.getMinutes()
+    console.log(minutes)
+    
+    const seconds = now.getSeconds()
+    console.log(seconds)
+
+    const timestamp = `${month} ${day}, ${year} at ${hours}:${minutes}`;
+  
+    return timestamp;
+}
+
+module.exports = timestamp;
+
+// ${periodOfDay}
+// const hours = now.getHours()
+// const periodOfDay
+//     if (now.getHours > 12) {
+//       hours= now.getHours() -12
+//       periodOfDay= "pm";
+//     } else {
+//       hours = now.getHours();
+//       periodOfDay = "am";
+//     }
+  
+ 
